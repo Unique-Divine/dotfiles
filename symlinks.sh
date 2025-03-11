b78a1f6843e423b92f570739a6ad3d4485796b8f
Unique Divine <realuniquedivine@gmail.com>
2025-03-11 15:13:38 -0500

    zsh: update gitignore and symlinks
    - Add "env.sh" to .gitignore to exclude environment configuration.
    - Update symlinks.sh to source "env.sh" and "bashlib.sh" for required environment variables.
    - Conditionally create symlink for nvim and view if both dependencies are available.

--- symlinks.sh
+++ symlinks.sh
@@ -8,6 +8,18 @@ ln -sf "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
 
 mkdir -p ~/.config
 ln -sf "$DOTFILES/nvim" ~/.config/
+source "$DOTFILES/env.sh" # For the $SUDO_PW env var
+source "$DOTFILES/zsh/bashlib.sh"
+
+# Ensure both nvim and view are available before creating symlink
+if which_ok nvim && which_ok view; then
+  # The `sudo` command doesn't typically read from stdin, so you have to use
+  # `sudo -S` to get the desired behavior. The `-S` is shorthand for `--std-in`.
+  # See the docs at `man sudo`.
+  echo "$SUDO_PW" | sudo -S ln -sf "$(which nvim)" "$(which view)"
+else
+    log_warning "Skipping symlink creation due to missing dependencies." >&2
+fi
 
 mkdir -p ~/.config/yarn/global
 ln -sf "$DOTFILES/.config/yarn/global/package.json" ~/.config/yarn/global/package.json
